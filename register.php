0a5cee10ce417dd5ab6178ee80dd6b61c0fb836b
AdityaGiradkar <51029130+AdityaGiradkar@users.noreply.github.com>
2020-10-11 02:06:47 +0530

    user register user name is added

--- register.php
+++ register.php
@@ -6,23 +6,24 @@
         if(isset($_POST['submit'])){
             //sanitize data
             $name   = mysqli_real_escape_string($con, $_POST['name']);
+            $u_name = mysqli_real_escape_string($con, $_POST['u_name']);
             $email  = mysqli_real_escape_string($con,$_POST['email']);
-            $phone  = mysqli_real_escape_string($con,$_POST['phone']);
+            // $phone  = mysqli_real_escape_string($con,$_POST['phone']);
             $pass   = mysqli_real_escape_string($con,$_POST['pass']);
 
-            $check_user = "SELECT * FROM `user` WHERE `email_id`='$email'";
+            $check_user = "SELECT * FROM `user` WHERE `user_name`='$u_name'";
             $check_user_run = mysqli_query($con, $check_user);
             $check_user_res = mysqli_num_rows($check_user_run);
 
             if($check_user_res == 0){
                 $vkey = md5(time().$name);
                 
-                $insert = "INSERT INTO `user`(`name`, `contact_no`, `email_id`, `password`, `vkey`) 
-                            VALUES ('$name', '$phone', '$email', '$pass', '$vkey')";
+                $insert = "INSERT INTO `user`(`name`, `user_name`, `email_id`, `password`, `vkey`) 
+                            VALUES ('$name', '$u_name', '$email', '$pass', '$vkey')";
                 $insert_run = mysqli_query($con, $insert);
 
                 if($insert_run){
-                    $get_user_id = "SELECT `user_id` FROM `user` WHERE `email_id`='$email'";
+                    $get_user_id = "SELECT `user_id` FROM `user` WHERE `user_name`='$u_name'";
                     $get_user_id_run = mysqli_query($con, $get_user_id);
                     $get_user_id_res = mysqli_fetch_assoc($get_user_id_run);
                     $user_id = $get_user_id_res['user_id'];
@@ -46,8 +47,8 @@
                 }
             }else{
                 echo "<script>
-                    alert('user already exist with email $email');
-                    window.location.href='register.php'; 
+                    alert('user already exist with user name $u_name');
+                    //window.location.href='register.php'; 
                 </script>";
             }
         }
@@ -79,25 +80,30 @@
                 <form method="post" action="">
                     <div class="form-group mt-5">
                         <!-- <label for="exampleInputEmail1">Username</label> -->
-                        <input type="text" class="form-control input-box" placeholder="Full Name" name="name"
-                            id="exampleInputEmail1">
+                        <input type="text" class="form-control input-box" placeholder="Full Name" value="<?php if(isset($_POST['name'])){ echo $_POST['name']; } ?>" name="name"
+                            id="exampleInputEmail1" required>
                     </div>
                     <div class="form-group">
                         <!-- <label for="exampleInputEmail1">Username</label> -->
-                        <input type="email" class="form-control input-box" placeholder="Email ID" name="email"
-                            id="exampleInputEmail1">
+                        <input type="text" class="form-control input-box" placeholder="User Name" name="u_name"
+                            id="exampleInputEmail1" required>
                     </div>
                     <div class="form-group">
-                        <!-- <label for="exampleInputPassword1">Password</label> -->
+                        <!-- <label for="exampleInputEmail1">Username</label> -->
+                        <input type="email" class="form-control input-box" placeholder="Email ID" value="<?php if(isset($_POST['email'])){ echo $_POST['email']; } ?>" name="email"
+                            id="exampleInputEmail1" required>
+                    </div>
+                    <!-- <div class="form-group">
+                    
                         <input type="text" class="form-control input-box" placeholder="Phone Number" name="phone"
                             id="exampleInputPassword1">
-                    </div>
+                    </div> -->
                     <div class="form-group">
                         <!-- <label for="exampleInputPassword1">Password</label> -->
-                        <input type="password" class="form-control input-box" placeholder="Password" name="pass"
-                            id="exampleInputPassword1">
+                        <input type="password" class="form-control input-box" placeholder="Create Password" name="pass"
+                            id="exampleInputPassword1" minlength="8" required>
                     </div>
-                    <button type="submit" class="login-btn mt-5 " name="submit">REGISTER</button>
+                    <button type="submit" class="login-btn mt-4 " style="color:white; " name="submit">REGISTER</button>
                 </form>
                 <small class="form-text text-center mt-3"><em>Already Registered ?</em> <a
                         href="login.php">Login</a></small>
@@ -122,7 +128,6 @@
 <?php
     }else{
         echo "<script>
-                    alert('Already Loged In..');
                     window.location.href='index.php';
                 </script>";
     }
