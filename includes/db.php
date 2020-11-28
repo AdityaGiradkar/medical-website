b43dffc16698106d922b15ef25189bc690094b2b
AdityaGiradkar <51029130+AdityaGiradkar@users.noreply.github.com>
2020-11-28 17:18:44 +0530

    added initials to name filed in register.php

--- includes/db.php
+++ includes/db.php
@@ -3,6 +3,12 @@
     $db['password'] = "Sadanand@123";
     $db['host']     = "localhost";
     $db['name']     = "medical_data";
+
+    // $db['user']     = "root";
+    // $db['password'] = "";
+    // $db['host']     = "localhost";
+    // $db['name']     = "medical";
+
     $con = mysqli_connect($db['host'], $db['user'], $db['password'], $db['name']);
 
     if (mysqli_connect_errno()) {
--- register.php
+++ register.php
@@ -5,11 +5,15 @@
         include("includes/db.php"); 
         if(isset($_POST['submit'])){
             //sanitize data
+            $initials = mysqli_real_escape_string($con, $_POST['initials']);
             $name   = mysqli_real_escape_string($con, $_POST['name']);
             $u_name = mysqli_real_escape_string($con, $_POST['u_name']);
             $email  = mysqli_real_escape_string($con,$_POST['email']);
             // $phone  = mysqli_real_escape_string($con,$_POST['phone']);
             $pass   = mysqli_real_escape_string($con,$_POST['pass']);
+
+            //combining initials with name
+            $name = $initials. " " .$name;
 
             $check_user = "SELECT * FROM `user` WHERE `user_name`='$u_name'";
             $check_user_run = mysqli_query($con, $check_user);
@@ -78,11 +82,29 @@
                 <img src="images/AtmaVeda Logo.png" class="mx-auto d-block" width="100" alt="AtmaVeda Logo" />
                 <p class="card-title mx-auto brand-name">AtmaVeda Yog</p>
                 <form method="post" action="">
-                    <div class="form-group mt-5">
-                        <!-- <label for="exampleInputEmail1">Username</label> -->
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <select class="btn btn-outline-secondary" name="initials" required>
+                                <option value="Mr.">Mr.</option>
+                                <option value="Mrs.">Mrs.</option>
+                                <option value="Miss.">Miss.</option>
+                                <option value="Mast.">Mast.</option>
+                            </select>
+                            <!-- <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">select</button>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="#">Mr.</a>
+                                <a class="dropdown-item" href="#">Mrs.</a>
+                                <a class="dropdown-item" href="#">Miss.</a>
+                                <a class="dropdown-item" href="#">Mast.</a>
+                            </div> -->
+                        </div>
+                        <input type="text" class="form-control input-box" placeholder="Enter Full Name"  value="<?php if(isset($_POST['name'])){ echo $_POST['name']; } ?>" name="name" required>
+                    </div>
+                    <!-- <div class="form-group mt-5">
+                     
                         <input type="text" class="form-control input-box" placeholder="Full Name" value="<?php if(isset($_POST['name'])){ echo $_POST['name']; } ?>" name="name"
                             id="exampleInputEmail1" required>
-                    </div>
+                    </div> -->
                     <div class="form-group">
                         <!-- <label for="exampleInputEmail1">Username</label> -->
                         <input type="text" class="form-control input-box" placeholder="User Name" name="u_name"
